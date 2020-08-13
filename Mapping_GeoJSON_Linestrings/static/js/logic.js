83be2fad8bce0b84c4594515fa20bfe08e2129e1
Connor Sparkman <connor.sparkman@looker.com>
2020-08-13 16:26:18 -0700

    13.5.5 complete

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,6 +1,12 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Create a style for the lines.
+let myStyle = {
+	color: "#ffffa1",
+	weight: 2
+}
+
 // We create the tile layer that will be the background of our map.
 let light = L.tileLayer(
   'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -28,24 +34,24 @@ let baseMaps = {
 let map = L.map('mapid', {
 	center: [44.0, -80.0],
 	zoom: 2,
-	layers: [light]
+	layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/csparkma/Mapping_Earthquakes/master/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/csparkma/berkeley-Mapping-Earthquakes/master/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
+    style: myStyle,
     // We turn each feature into a marker on the map.
     onEachFeature: (feature, layer) => {
-      console.log(layer);
-      layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
+      layer.bindPopup("<h3>airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
     }
   }).addTo(map);
 });
